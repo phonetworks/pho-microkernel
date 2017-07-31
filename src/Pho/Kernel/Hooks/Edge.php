26a09b0e6e5722cfe1ff284a0f78455779f78362
Groups <esokullu@gmail.com>
2017-07-30 21:11:59 -0700

    bugfix

--- src/Pho/Kernel/Hooks/Edge.php
+++ src/Pho/Kernel/Hooks/Edge.php
@@ -21,15 +21,15 @@ class Edge implements HookInterface
     public static function setup(/*Graph\EdgeInterface*/ $edge): void
     {
         $edge->hook("head", (function(): Graph\NodeInterface  {
-                $this->head = 
-                $this->injection("kernel")->gs()->node($this->head_id);
+                $this->head = new \Pho\Lib\Graph\HeadNode();
+                $this->head->set($this->injection("kernel")->gs()->node($this->head_id));
                 return $this->head;
             })
         );
 
         $edge->hook("tail", (function(): Graph\NodeInterface {
-                $this->tail =  
-                $this->injection("kernel")->gs()->node($this->tail_id);
+                $this->tail =  new \Pho\Lib\Graph\TailNode();
+                $this->tail->set($this->injection("kernel")->gs()->node($this->tail_id));
                 return $this->tail;
             })
         );
