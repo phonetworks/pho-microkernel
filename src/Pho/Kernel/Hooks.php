e70e17fd90dd910800a89d37f7915287eed8f4a9
Emre Sokullu <esokullu@gmail.com>
2017-08-29 15:33:04 +0300

    Documentation

--- src/Pho/Kernel/Hooks.php
+++ src/Pho/Kernel/Hooks.php
@@ -5,8 +5,27 @@ namespace Pho\Kernel;
 use Pho\Lib\Graph;
 use Pho\Framework\AbstractNotification;
 
+/**
+ * Hooks class.
+ *
+ * As its name implies, this class hooks objects (node, edge, graph or notification)
+ * with theirs hydrator methods. To illustrate, a node object that has just been 
+ * restored from the database would have its creator_id set up, but not its creator 
+ * object. That is because, creator, as an object, is not stored in the database. 
+ * Hooks hydrate this object, that is currently in its flat form, by fetching these 
+ * associated objects based on their IDs.
+ *
+ * @author Emre Sokullu <emre@phonetworks.org>
+ */
 class Hooks
 {
+    /**
+     * Detects the object type and calls the right Hooks class.
+     *
+     * @param mixed $obj The object.
+     *
+     * @return void
+     */
     public static function setup(/* mixed */ $obj): void
     {
         if($obj instanceof Graph\GraphInterface) {
