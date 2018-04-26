01e6c30aeeb40e4a70e690ced3f5a6a5ce6804fe
Groups <esokullu@gmail.com>
2018-04-25 22:05:55 -0700

    2.2.12

--- src/Pho/Kernel/Foundation/AttributeBag.php
+++ src/Pho/Kernel/Foundation/AttributeBag.php
@@ -15,11 +15,36 @@ class AttributeBag extends LibGraph\AttributeBag
     public function __construct(LibGraph\EntityInterface $owner, array $bag = []) 
     {
         parent::__construct($owner, $bag);
-        $this->hydrate();
+        //$this->hydrate(); // disabled because it was creating issues
+    }
+
+    public function __get(string $attribute)
+    {
+        if(!isset($this->$attribute)) {
+            return null;
+        }
+        $value = $this->bag[$attribute];
+        if(!is_string($value)) {
+            return $value;
+        }
+        $s = S::create($value);
+            if(
+                $s->startsWith(Kernel::PARTICLE_IN_ATTRIBUTEBAG_TPL[0])
+                &&
+                $s->endsWith(Kernel::PARTICLE_IN_ATTRIBUTEBAG_TPL[1])
+            ) {
+                $id = (string) $s
+                        ->removeLeft(Kernel::PARTICLE_IN_ATTRIBUTEBAG_TPL[0])
+                        ->removeRight(Kernel::PARTICLE_IN_ATTRIBUTEBAG_TPL[1]);
+                $this->bag[$attribute] = $this->owner->kernel()->gs()->node($id);
+            }
+        return $this->bag[$attribute];
     }
 
     /**
      * Hydrates the imported values.
+     * 
+     * !! unusued !! it was creating when in __construct
      *
      * @return void
      */
